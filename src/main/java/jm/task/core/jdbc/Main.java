9a03fdd7782010678fdad63058413b6e7d8a3cb2
GelbeDuck <superserge.k@gmail.com>
2024-01-29 22:10:37 +0300

    bug fix)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-//        Util.getConnection();
         UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
